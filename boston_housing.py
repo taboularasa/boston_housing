fbd26f214318cf06264e65ecf222bc5403c9f273
David Elliott <david@philosophie.is>
2016-01-03 16:22:46 -0800

    remove extra white space

--- boston_housing.py
+++ boston_housing.py
@@ -186,7 +186,7 @@ def fit_predict_model(city_data):
     # Fit the learner to the training data to obtain the best parameter set
     print "Final Model: "
     print reg.fit(X, y)
-    
+
     # Use the model to predict the output of a particular sample
     x = [11.95, 0.00, 18.100, 0, 0.6590, 5.6090, 90.00, 1.385, 24, 680.0, 20.20, 332.09, 12.13]
     y = reg.predict(x)
